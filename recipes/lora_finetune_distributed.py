2435821898cc62dba1d78f46c7949a1833b2dc4c
Vivek <vgoel@habana.ai>
2024-12-18 12:25:53 +0200

    Add HPU as new device
    Add yaml for single device LoRA with HPU
    
    Update yaml for single device LoRA with HPU
    
    Set device for lm_eval
    
    Add lm_eval configs for HPU
    
    Support distributed training in Gaudi
    
    Distributed Finetuning on HPU
    
    Keeping original changes
    
    Re-Enable profiler activities for XPU
    
    Fix assert at end of training
    
    delete hpu eval configs
    
    fix lora recipe after rebase
    
    Add HPU support to Profiler

--- recipes/lora_finetune_distributed.py
+++ recipes/lora_finetune_distributed.py
@@ -485,6 +485,20 @@ class LoRAFinetuneRecipeDistributed(FTRecipeInterface):
                 model, auto_wrap_policy={modules.TransformerSelfAttentionLayer}
             )
 
+        if torch.hpu.is_available():
+            # Initialize LoRA params and RoPE buffers (Before FSDP sharding)
+            with training.set_default_dtype(self._dtype), self._device:
+                lora_device = "cpu" if fsdp_cpu_offload else self._device
+                for m in model.modules():
+                    if (isinstance(m, AdapterModule)) and not lora_weights_state_dict:
+                        # lora may not be covered in state dict
+                        # if finetune for the 1st time
+                        m.to_empty(device=lora_device)
+                        m.initialize_parameters()
+
+                    if hasattr(m, "rope_init"):
+                        m.rope_init()
+
         # For FSDP sharding
         fsdp_shard_conditions = [
             partial(
@@ -509,18 +523,19 @@ class LoRAFinetuneRecipeDistributed(FTRecipeInterface):
         else:
             lora_missing, lora_unexpected = None, None
 
-        # Initialize LoRA params and RoPE buffers
-        with training.set_default_dtype(self._dtype), self._device:
-            lora_device = "cpu" if fsdp_cpu_offload else self._device
-            for m in model.modules():
-                if (isinstance(m, AdapterModule)) and not lora_weights_state_dict:
-                    # lora may not be covered in state dict
-                    # if finetune for the 1st time
-                    m.to_empty(device=lora_device)
-                    m.initialize_parameters()
+        if torch.cuda.is_available():
+            # Initialize LoRA params and RoPE buffers
+            with training.set_default_dtype(self._dtype), self._device:
+                lora_device = "cpu" if fsdp_cpu_offload else self._device
+                for m in model.modules():
+                    if (isinstance(m, AdapterModule)) and not lora_weights_state_dict:
+                        # lora may not be covered in state dict
+                        # if finetune for the 1st time
+                        m.to_empty(device=lora_device)
+                        m.initialize_parameters()
 
-                if hasattr(m, "rope_init"):
-                    m.rope_init()
+                    if hasattr(m, "rope_init"):
+                        m.rope_init()
 
         base_missing, base_unexpected = training.load_from_full_model_state_dict(
             model,
